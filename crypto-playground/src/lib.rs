94378d1fe6e3fa5f99597ec879bb2eae25edcf0a
Shai Caspin <cashai99@gmail.com>
2024-04-09 22:24:29 -0400

    convert full sha256 impl

--- crypto-playground/src/lib.rs
+++ crypto-playground/src/lib.rs
@@ -1,22 +1,143 @@
 use bums_macros;
 use round::round_down;
 
-fn sha256(data: *const u8, len: usize, out: *mut u8) -> *mut u8 {
-    // let mut ctx = SHA256_CTX::init();
-    // let ctx_pointer = &mut ctx;
-    // let update_result = SHA256_Update(ctx_pointer, data, len);
-    // let final_result = SHA256_Final(out, ctx_pointer);
-    //    if init_result && update_result && final_result {
-    //        ();
-    //    } else {
-    //        ();
-    //    }
-    //openssl_cleanse
-    out
+const SHA256_DIGEST_LENGTH: usize = 32;
+const SHA256_CBLOCK: usize = 64;
+
+#[allow(non_camel_case_types)]
+type SHA256_CTX = Sha256StateSt;
+
+struct Sha256StateSt {
+    h: [u32; 8],
+    nl: u32,
+    nh: u32,
+    data: [u8; SHA256_CBLOCK],
+    num: usize,
+    md_len: usize,
 }
 
+impl Sha256StateSt {
+    fn init() -> Self {
+        Self {
+            h: [
+                0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab,
+                0x5be0cd19,
+            ],
+            nl: 0,
+            nh: 0,
+            data: [0; SHA256_CBLOCK],
+            num: 0,
+            md_len: SHA256_DIGEST_LENGTH,
+        }
+    }
+}
+
+fn ms_memcpy(dst: &mut [u8], src: &[u8], n: usize) {
+    if n == 0 {
+        return;
+    }
+    dst[0..n].copy_from_slice(&src[0..n]);
+}
+
+fn ms_memset(dst: &mut [u8], c: u8, n: usize) {
+    if n == 0 {
+        return;
+    }
+    dst[0..n].fill(c);
+}
+
+fn sha256_update(ctx: &mut SHA256_CTX, msg: &[u8], len: usize) -> Result<(), ()> {
+    //call to crypt_md32_update
+    let mut len = len;
+    let mut msg = msg;
+
+    if len == 0 {
+        return Ok(());
+    }
+
+    let l = ctx.nl + ((len << 3) as u32);
+    if l < ctx.nl {
+        ctx.nh = ctx.nh + 1;
+    }
+    ctx.nh = ctx.nh + ((len >> 29) as u32);
+    ctx.nl = l;
+
+    let mut n = ctx.num;
+    if n != 0 {
+        if len > SHA256_CBLOCK || len + n >= SHA256_CBLOCK {
+            ms_memcpy(&mut ctx.data[n..], msg, SHA256_CBLOCK - n);
+            sha256_block_data_order(&mut ctx.h, &ctx.data, 1);
+            n = SHA256_CBLOCK - n;
+            msg = &msg[n..];
+            len = len - n;
+            ctx.num = 0;
+            ms_memset(&mut ctx.data, 0, SHA256_CBLOCK);
+        } else {
+            ms_memcpy(&mut ctx.data[n..], msg, len);
+            ctx.num = ctx.num + (len as usize);
+            return Ok(());
+        }
+    }
+
+    n = len / SHA256_CBLOCK;
+    if n > 0 {
+        sha256_block_data_order(&mut ctx.h, msg, n);
+        n = n * SHA256_CBLOCK;
+        msg = &msg[n..];
+        len = len - n;
+    }
+
+    if len != 0 {
+        ctx.num = len as usize;
+        ms_memcpy(&mut ctx.data, msg, len);
+    }
+
+    Ok(())
+}
+
+fn sha256_final(out: &mut [u8], ctx: &mut SHA256_CTX) -> Result<(), ()> {
+    // call to crypto_md32_final
+    let mut n = ctx.num;
+    //assert!(n<SHA256_CBLOCK)
+    ctx.data[n] = 0x80;
+    n = n + 1;
+
+    if n > (SHA256_CBLOCK - 8) {
+        ms_memset(&mut ctx.data[n..], 0, SHA256_CBLOCK - n);
+        n = 0;
+        sha256_block_data_order(&mut ctx.h, &ctx.data, 1);
+    }
+    ms_memset(&mut ctx.data[n..], 0, SHA256_CBLOCK - 8 - n);
+
+    // Append a 64-bit length to the block and process it.
+    // is big endian = true
+    ctx.data[SHA256_CBLOCK - 8] = ctx.nh as u8; // FIX: not sure if right
+    ctx.data[SHA256_CBLOCK - 4] = ctx.nl as u8;
+
+    sha256_block_data_order(&mut ctx.h, &ctx.data, 1);
+    ctx.num = 0;
+    ms_memset(&mut ctx.data, 0, SHA256_CBLOCK);
+
+    if ctx.md_len != SHA256_DIGEST_LENGTH {
+        return Err(());
+    }
+
+    out.copy_from_slice(&convert(&ctx.h));
+    Ok(())
+}
+
+fn sha256(data: &[u8], len: usize, out: &mut [u8]) {
+    let mut ctx = SHA256_CTX::init();
+
+    // TODO: handle results
+    let _ = sha256_update(&mut ctx, data, len);
+    let _ = sha256_final(out, &mut ctx);
+}
+
+// straight out of aws-cl-rs
+// https://github.com/aws/aws-lc-rs/blob/0d8ef6cf53429cfabadbde73a986bd3528054178/aws-lc-rs/src/digest/sha.rs#L212
 pub fn sha256_digest(msg: &[u8], output: &mut [u8]) {
-    sha256(msg.as_ptr(), msg.len(), output.as_mut_ptr());
+    sha256(msg, msg.len(), output);
 }
 
 fn convert(data: &[u32; 8]) -> [u8; 32] {
@@ -34,12 +155,11 @@ pub fn incomplete_sha256_digest(msg: &[u8], output: &mut [u8]) {
     ];
 
     let length = round_down((msg.len() / 64) as f64, 0) as usize;
-    println!("length: {:?}", length.clone());
     sha256_block_data_order(context, msg, length);
     output.copy_from_slice(&convert(context));
 }
 
-#[bums_macros::check_mem_safe("assembly/processed_sha256_asm.S", context.as_mut_ptr(), input.as_ptr(), input_len)]
+#[bums_macros::check_mem_safe("assembly/sha256-armv8-apple.S", context.as_mut_ptr(), input.as_ptr(), input_len)]
 fn sha256_block_data_order(context: &mut [u32; 8], input: &[u8], input_len: usize);
 
 //#[bums_macros::check_mem_safe("assembly/processed_sha256_asm.S", context.as_mut_ptr(), input.as_mut_ptr(), input.len())]
@@ -99,6 +219,12 @@ mod tests {
         let mut v = vec![0; 32];
         let mut output: &mut [u8] = v.as_mut_slice();
         sha256_digest(&message, &mut output);
-        assert!(output != message);
+        assert_eq!(
+            output,
+            [
+                165, 148, 238, 222, 185, 126, 9, 39, 104, 10, 145, 195, 176, 0, 227, 248, 125, 98,
+                238, 3, 49, 147, 246, 175, 241, 168, 84, 157, 37, 223, 105, 41
+            ]
+        );
     }
 }
