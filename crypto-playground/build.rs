392941b37dc52a359c0ad1ef65cc67993ff52436
Amit Aryeh Levy <amit@amitlevy.com>
2024-04-10 09:42:17 -0400

    Switch to Linux aarch64 SHA256 and fix implementation
    Pull amit's fixes to sha

--- crypto-playground/build.rs
+++ crypto-playground/build.rs
@@ -1,19 +1,10 @@
 fn main() {
     // Compile your assembly code
-    println!("cargo:rerun-if-changed=assembly/sha256-armv8-apple.S");
+    println!("cargo:rerun-if-changed=assembly/sha256-armv8-linux.S");
     cc::Build::new()
-        .file("assembly/sha256-armv8-apple.S")
+        .file("assembly/sha256-armv8-linux.S")
         .compile("unique_name_for_this_lib_sha256");
     // Link the generated object file with the Rust project
-    println!("cargo:rustc-link-object=sha256-armv8-apple.o");
+    println!("cargo:rustc-link-object=sha256-armv8-linux.o");
     println!("cargo:rustc-link-lib=unique_name_for_this_lib_sha256");
-
-    // Compile your assembly code
-    println!("cargo:rerun-if-changed=assembly/sha1-armv8-apple.S");
-    cc::Build::new()
-        .file("assembly/sha1-armv8-apple.S")
-        .compile("sha1");
-    // Link the generated object file with the Rust project
-    println!("cargo:rustc-link-object=sha1-armv8-apple.o");
-    println!("cargo:rustc-link-lib=sha1");
 }
