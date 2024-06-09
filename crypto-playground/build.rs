486612a406c81ca97c0e9eea3ea876a8cfb14da5
Shai Caspin <cashai99@gmail.com>
2024-06-09 18:02:08 -0400

    use ios asm for shai's m1 mac

--- crypto-playground/build.rs
+++ crypto-playground/build.rs
@@ -4,29 +4,39 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=assembly/aws-fips");
     let mut build = cc::Build::new();
 
-    let os = match env::var("CARGO_CFG_TARGET_OS")?.as_str() {
-	"windows" => "win",
-	"linux" => "linux",
-	"ios" => "ios",
-	"macos" => "mac",
-	s => panic!("Unsupported operating system {}", s)
+    let mut os = match env::var("CARGO_CFG_TARGET_OS")?.as_str() {
+        "windows" => "win",
+        "linux" => "linux",
+        "ios" => "ios",
+        "macos" => "mac",
+        s => panic!("Unsupported operating system {}", s),
     };
 
     let arch = env::var("CARGO_CFG_TARGET_ARCH")?;
+
+    // (scaspin) special case to run on m1 mac, unsupported by aws-lc-rs
+    if os == "mac" && arch == "aarch64" {
+        os = "ios"
+    }
+
     for entry in std::fs::read_dir(format!("generated-asm/{}-{}/crypto/fipsmodule", os, arch))? {
         match entry {
             Ok(entry) => {
-		println!("cargo::rerun-if-changed={}", entry.path().display());
-		let mut processed_path = std::env::var_os("OUT_DIR").map(std::path::PathBuf::from).ok_or(std::io::Error::last_os_error())?;
-		processed_path.push(entry.file_name());
-		File::create(processed_path.clone())?.write_all(
-		    &cc::Build::new()
-			// TODO(alevy): remove this define once we can
-			// parse the OPENSSL_armcap_P extern variable
-			// in the SHA256 and other assembly
-			.define("__KERNEL__", "1")
-			.include("include").file(entry.path()).expand()
-		)?;
+                println!("cargo::rerun-if-changed={}", entry.path().display());
+                let mut processed_path = std::env::var_os("OUT_DIR")
+                    .map(std::path::PathBuf::from)
+                    .ok_or(std::io::Error::last_os_error())?;
+                processed_path.push(entry.file_name());
+                File::create(processed_path.clone())?.write_all(
+                    &cc::Build::new()
+                        // TODO(alevy): remove this define once we can
+                        // parse the OPENSSL_armcap_P extern variable
+                        // in the SHA256 and other assembly
+                        .define("__KERNEL__", "1")
+                        .include("include")
+                        .file(entry.path())
+                        .expand(),
+                )?;
                 build.file(processed_path);
             }
             Err(e) => println!("cargo::warning:{}", e),
